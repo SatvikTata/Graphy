8db89df7982b3d45ce0376f1abde0862eb4fee49
Satvik <iamsatvik77@gmail.com>
2022-05-29 15:13:03 +0530

    v0.2

--- Graphy.py
+++ Graphy.py
@@ -5,11 +5,32 @@ import numpy as np
 preX=0
 preY=0
 
+lastpreX=0
+lastpreY=0
+
 x=5
 y=int(input("Enter the total amount: "))
 
 xpoints = np.array([preX,x])
 ypoints = np.array([preY,y])
 
+updateTrue = 0
+updateTrue=int(input("Do you want to update the graph: "))
+
+if(updateTrue==1):
+    
+    lastpreX=preX
+    lastpreY=preY
+    preX=x
+    preY=y
+
+    x=6
+    y=int(input("Enter the total amount: "))
+
+    xpoints=np.array([lastpreX,preX,x])
+    ypoints=np.array([lastpreY,preY,y])
+
+
 plt.plot(xpoints,ypoints)
 plt.show()
+
